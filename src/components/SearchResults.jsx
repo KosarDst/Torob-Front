444c358433bf9c80167a27d21766786ba6d6483f
Kosar Dastbaz <72091169+KosarDst@users.noreply.github.com>
2022-07-02 01:34:17 +0430

    Add files via upload

---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,216 @@
+import React, { useState, useEffect } from "react";
+import "../assets/css/SearchResults.css";
+import PageHeader from "../page/PageHeader";
+import ResultCard from "./ResultCard";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const SearchResults = () => {
+  const params = useParams();
+  /*  */
+  const [priceFilter, setPriceFilter] = useState(false);
+  const [dropFilter, setDropFilter] = useState(false);
+  const [isSideFilter, setIsSideFilter] = useState(false);
+  /*  */
+  const [allResultProd, setAllResultProd] = useState([]);
+  const [price, setPrice] = useState({
+    min: "0",
+    max: "500000",
+  });
+  const [sortFilter, setSortFilter] = useState("publish_date");
+
+  useEffect(() => {
+    let hasData = false;
+
+    if (!hasData) {
+      const currUser = localStorage.getItem("userDetail");
+      if (
+        params.category === "samsung" ||
+        params.category === "xiaomi" ||
+        params.category === "apple" ||
+        params.category === "asus" ||
+        params.category === "lenovo"
+      ) {
+        axios
+          .get(
+            `http://localhost:8000/api/v0/post/list/all?search=${params.category}&ordering=publish_date`,
+            currUser
+          )
+          .then((info) => {
+            setAllResultProd(info.data.data);
+          })
+          .catch((err) => {
+            return err;
+          });
+      } else {
+        axios
+          .get(
+            `http://localhost:8000/api/v0/post/list/${params.category}?ordering=publish_date`,
+            currUser
+          )
+          .then((info) => {
+            setAllResultProd(info.data.data);
+          })
+          .catch((err) => {
+            return err;
+          });
+      }
+    }
+
+    return () => {
+      hasData = true;
+    };
+  }, []);
+
+  const handleChangePrice = (e) => {
+    const { name, value } = e.target;
+    setPrice((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
+  const handleSubmitPriceFilter = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `http://localhost:8000/api/v0/post/list/${params.category}?from_price=${price.min}&to_price=${price.max}&ordering=publish_date`
+      )
+      .then((info) => setAllResultProd(info.data.data));
+  };
+  const handleSubmitSortFilter = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `http://localhost:8000/api/v0/post/list/${params.category}?ordering=${sortFilter}`
+      )
+      .then((info) => setAllResultProd(info.data.data));
+  };
+
+  return (
+    <div className="searchresult">
+      <PageHeader />
+      <div className="main-data">
+        <i
+          className={`fa-solid handle-side-filter ${
+            isSideFilter ? "fa-xmark" : "fa-bars"
+          }`}
+          onClick={() => {
+            setIsSideFilter(!isSideFilter);
+          }}
+        ></i>
+        <div
+          className={`filter-data ${isSideFilter ? "open-side-filter" : null}`}
+        >
+          <div className="category-filter">
+            <div>
+              <h3>کیف و کاور گوشی</h3>
+            </div>
+            <div>
+              <h3>محافط صحفه نمایش گوشی</h3>
+            </div>
+            <div>
+              <h3>پایه نگهدارنده گوشی و تبلت</h3>
+            </div>
+            <div>
+              <h3>سایر قطعات گوشی و تبلت</h3>
+            </div>
+            <div>
+              <h3>نمایش تمام دسته بندی ها</h3>
+            </div>
+          </div>
+          {/* split line */}
+          <div className="split-line"></div>
+          <div className="price-filter">
+            <div className="main-price">
+              <h3>قیمت (تومان)</h3>
+              <i
+                className={`fa-solid fa-angle-down price-angle ${
+                  priceFilter ? "rotate-angle" : null
+                }`}
+                onClick={() => {
+                  setPriceFilter(!priceFilter);
+                }}
+              ></i>
+            </div>
+            <div className={`price-data ${priceFilter ? "openPrice" : null}`}>
+              <div className="price-inputs">
+                <input
+                  onChange={handleChangePrice}
+                  type="number"
+                  name="min"
+                  placeholder="از"
+                  value={price.min}
+                />
+                <input
+                  onChange={handleChangePrice}
+                  type="number"
+                  name="max"
+                  placeholder="تا"
+                  value={price.max}
+                />
+              </div>
+              <button onClick={handleSubmitPriceFilter}>اعمال</button>
+            </div>
+          </div>
+        </div>
+        <div className="result-data">
+          <div className="drop-filter">
+            <div className="dropdown">
+              <h2>مرتب سازی</h2>
+              <i
+                className={`fa-solid fa-angle-down drop-angle ${
+                  dropFilter ? "openDrop" : null
+                }`}
+                onClick={() => {
+                  setDropFilter(!dropFilter);
+                }}
+              ></i>
+            </div>
+            <ul className={`items  ${dropFilter ? "opendropFilter" : null}`}>
+              <li
+                className="item"
+                onClick={() => {
+                  setSortFilter("cheapest");
+                  handleSubmitSortFilter();
+                }}
+              >
+                ارزان ترین
+              </li>
+              <li
+                className="item"
+                onClick={() => {
+                  setSortFilter("the_most_expensive");
+
+                  handleSubmitSortFilter();
+                }}
+              >
+                گران ترین
+              </li>
+              <li
+                className="item"
+                onClick={() => {
+                  setSortFilter("publish_date");
+                  handleSubmitSortFilter();
+                }}
+              >
+                جدید ترین
+              </li>
+            </ul>
+          </div>
+          {/*  */}
+          <div className="cards">
+            {allResultProd && Array.isArray(allResultProd)
+              ? allResultProd.map((prod) => {
+                  return <ResultCard key={prod.id} cardData={prod} />;
+                })
+              : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResults;
---
+++ src/features/userSlice.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: {
+      username: localStorage.getItem("username"),
+      email: localStorage.getItem("email"),
+      isLogged: true,
+    },
+    products: [],
+    favs: [],
+    currentItem: null,
+  },
+  reducers: {
+    signup: (state, action) => {
+      localStorage.setItem("username", action.payload.username);
+      localStorage.setItem("email", action.payload.email);
+      state.user = {
+        username: localStorage.getItem("username"),
+        email: localStorage.getItem("email"),
+        isLogged: true,
+      };
+      loadCurrUser(action.payload);
+    },
+    signin: (state, action) => {
+      localStorage.setItem("username", action.payload.username);
+      localStorage.setItem("email", action.payload.email);
+      state.user = {
+        username: localStorage.getItem("username"),
+        email: localStorage.getItem("email"),
+        isLogged: true,
+      };
+      loadCurrUser(action.payload);
+    },
+    signout: (state) => {
+      state.user = {
+        username: null,
+        email: null,
+        isLogged: false,
+      };
+    },
+    loadAllProducts: (state, action) => {
+      state.products = action.payload;
+    },
+    loadCurrentItem: (state, action) => {
+      state.currentItem = action.payload;
+    },
+  },
+});
+
+export const { signup, signin, signout, loadAllProducts, loadCurrentItem } =
+  userSlice.actions;
+
+export const selectUser = (state) => state.user.user;
+export const allProduct = (state) => state.user.products;
+export const loadCurrUser = (user) => {
+  localStorage.setItem("userDetail", JSON.stringify(user));
+  let currenUser = localStorage.getItem("userDetail");
+  axios
+    .get("http://localhost:8000/api/v0/auth/signin", currenUser)
+    .then((info) => {
+      return info;
+    });
+};
+
+export default userSlice.reducer;
